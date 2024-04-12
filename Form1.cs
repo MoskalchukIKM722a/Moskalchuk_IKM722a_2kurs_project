af879163db98a66c53c04a6f642f69bec3ea4967
Олександр Москальчук <Oleksandr.Moskalchuk@infiz.khpi.edu.ua>
2024-04-12 13:36:46 +0300

    Finish task number 1

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,60 @@ namespace Moskalchuk_IKM722a_2kurs_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show ("Attention, it's been 30 seconds");
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tbInput.Enabled = true;// Режим дозволу
+                tbInput.Focus();
+                tClock.Start();
+                bStart.Text = "Stop"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+            }
+            else
+            {
+                tbInput.Enabled = false;// Режим заборони введення
+                tClock.Stop();
+                bStart.Text = "Start";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+            }
+        }
+
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Wrong character", "Misstake");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
